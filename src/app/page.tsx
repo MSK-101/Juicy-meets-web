b16c7c84711ab5d7d242cbbb3a9283a507c74b47
MSK-101 <msk98.sk@gmail.com>
2025-07-11 05:20:11 +0500

    fix the home page for desktop and mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ import LoginDialog from "@/components/dialogs/login-dialog";
 import UserDetailsDialog from "@/components/dialogs/user-details-dialog";
 import { useRouter } from "next/navigation";
 import PlansDialog from "@/components/dialogs/plans-dialog";
-import ImageSlider from "../components/ui/ImageSlider";
+import ImageSlider from "../components/ui/image-slider";
+import MobileImageSlider from "@/components/ui/mobile-image-slider";
 
 export default function Home() {
   const [showLoginDialog, setShowLoginDialog] = useState(false);
@@ -31,8 +32,8 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-[url('/home/hero_bg.png')] bg-cover bg-center bg-no-repeat">
-      <header className="flex justify-between items-center bg-black/10 backdrop-blur-xs shadow-2xl p-5 lg:px-14 lg:py-8">
+    <div className="min-h-screen flex flex-col md:bg-[url('/home/hero_bg.png')] md:bg-cover md:bg-center md:bg-no-repeat relative bg-radial-[at_50%_50%] from-[#A866FF] from-20% via-[#8B33FF] via-40% to-[#0B001A] to-70%">
+      <header className="flex justify-between items-center md:bg-black/10 md:backdrop-blur-xs md:shadow-2xl p-5 lg:px-14 lg:py-8 z-10">
         <Link href="/">
           <Image
             src="/logo.png"
@@ -42,8 +43,8 @@ export default function Home() {
             className="h-8 w-auto cursor-pointer hover:scale-105 duration-300"
           />
         </Link>
-        <div className="flex items-center gap-4 md:gap-14">
-          <div className="text-base md:text-lg hidden lg:block">
+        <div className="flex items-center md:gap-14">
+          <div className="hidden md:block text-lg">
             <span className="font-semibold">3,435,789</span> Online Video
             Chatters
           </div>
@@ -63,16 +64,9 @@ export default function Home() {
         </div>
       </header>
 
-      {/* Counter for mobile */}
-      <div className="text-base font-medium text-center my-12 lg:hidden">
-        3,435,789 Online Video Chatters
-      </div>
-
-      <ImageSlider imageCount={4} basePath="/home/hero_img_" />
-
-      <div className="flex flex-col items-center text-center px-5 mt-4 lg:mt-6">
+      <main className="mt-auto mb-10 flex flex-col items-center justify-center z-20">
         <button
-          className="py-6 px-12 rounded-full font-bold text-base md:text-xl my-6 cursor-pointer duration-300 hover:scale-105"
+          className="py-6 px-12 rounded-full font-bold text-base md:text-xl my-6 cursor-pointer duration-300 hover:scale-105 order-3 md:order-1"
           style={{
             background:
               "linear-gradient(90deg, #a91dfd 0%, #b231f5 25%, #a91dfd 50%, #c66cf3 85%, #de48ff 100%)",
@@ -96,14 +90,24 @@ export default function Home() {
           setShowDialog={setShowPlansDialog}
         />
 
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-[3rem] my-4">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-[3rem] my-4 text-center order-1 md:order-2">
           Welcome to Juicy Meets Chats.
         </h1>
 
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl max-w-[700px] leading-relaxed animate-in fade-in slide-in-from-bottom duration-500 delay-300">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl max-w-[700px] leading-relaxed animate-in fade-in slide-in-from-bottom duration-500 delay-300 text-center order-2 md:order-3">
           Connecting people worldwide to build meaningful relationships across
           cultures and borders.
         </p>
+      </main>
+
+      <div className="h-full absolute  top-0 left-0 right-0 overflow-hidden">
+        <div className="hidden md:block pt-40">
+          <ImageSlider imageCount={4} basePath="/home/hero_img_" />
+        </div>
+
+        <div className="block md:hidden scale-120">
+          <MobileImageSlider imageCount={4} basePath="/home/hero_img_" />
+        </div>
       </div>
     </div>
   );
